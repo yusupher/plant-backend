c609c5e7daf4080d51e66834a4bf696fc9540dfd
yusupher <olucast61@gmail.com>
2026-04-26 20:50:40 +0100

    Update cropdata.js

--- cropdata.js
+++ cropdata.js
@@ -1,317 +1,52 @@
-const crops = [
+module.exports = [
+  { name:"Cassava", temp:[20,35], rain:[1000,3000], ph:[4.5,7.5], zones:["All"], yield:25, fertilizer:"NPK 15-15-15 + Manure", pestResistanceScore:2, waterNeedScore:2 },
+  { name:"Maize", temp:[18,32], rain:[500,1500], ph:[5.5,7.0], zones:["Savanna","Guinea Savanna"], yield:5.5, fertilizer:"NPK + Urea top dressing", pestResistanceScore:2, waterNeedScore:3 },
+  { name:"Rice", temp:[20,35], rain:[1200,3000], ph:[5.0,6.5], zones:["Rainforest","Mangrove"], yield:4.2, fertilizer:"NPK 20-10-10 + Urea", pestResistanceScore:1, waterNeedScore:5 },
+  { name:"Yam", temp:[25,30], rain:[1000,2000], ph:[5.5,7.0], zones:["Forest","Guinea Savanna"], yield:15, fertilizer:"NPK + Organic compost", pestResistanceScore:3, waterNeedScore:3 },
+  { name:"Sorghum", temp:[25,35], rain:[300,1000], ph:[5.0,8.0], zones:["Sahel","Savanna"], yield:3, fertilizer:"NPK 100kg/ha", pestResistanceScore:4, waterNeedScore:2 },
+  { name:"Millet", temp:[25,40], rain:[200,800], ph:[4.5,7.5], zones:["Sahel"], yield:1.5, fertilizer:"Microdose NPK", pestResistanceScore:5, waterNeedScore:1 },
+  { name:"Cowpea", temp:[20,35], rain:[500,1200], ph:[5.0,7.5], zones:["Savanna","Guinea Savanna"], yield:1.5, fertilizer:"SSP + Rhizobium inoculation", pestResistanceScore:4, waterNeedScore:2 },
+  { name:"Soybean", temp:[20,30], rain:[600,1200], ph:[5.5,7.0], zones:["Savanna"], yield:2.2, fertilizer:"Inoculant + SSP", pestResistanceScore:3, waterNeedScore:2 },
+  { name:"Groundnut", temp:[20,32], rain:[500,1200], ph:[5.5,7.0], zones:["Savanna"], yield:2.0, fertilizer:"Gypsum + SSP", pestResistanceScore:3, waterNeedScore:2 },
+  { name:"Sweet Potato", temp:[20,30], rain:[750,1250], ph:[5.0,6.5], zones:["All"], yield:12, fertilizer:"NPK 15-15-15", pestResistanceScore:3, waterNeedScore:3 },
 
-/* ================= ROOT CROPS ================= */
+  { name:"Tomato", temp:[18,28], rain:[600,1200], ph:[5.5,7.5], zones:["All"], yield:20, fertilizer:"NPK + Calcium nitrate", pestResistanceScore:1, waterNeedScore:3 },
+  { name:"Pepper", temp:[20,30], rain:[600,1200], ph:[5.5,7.0], zones:["All"], yield:10, fertilizer:"NPK + Potassium boost", pestResistanceScore:2, waterNeedScore:3 },
+  { name:"Okra", temp:[22,35], rain:[600,1500], ph:[6.0,7.5], zones:["All"], yield:8, fertilizer:"NPK light application", pestResistanceScore:3, waterNeedScore:2 },
+  { name:"Cucumber", temp:[18,30], rain:[800,1200], ph:[5.5,7.0], zones:["All"], yield:15, fertilizer:"NPK + Compost", pestResistanceScore:2, waterNeedScore:3 },
+  { name:"Watermelon", temp:[25,35], rain:[500,1000], ph:[5.5,7.0], zones:["Savanna"], yield:25, fertilizer:"NPK + Potassium", pestResistanceScore:2, waterNeedScore:3 },
 
-{
-  name: "Cassava",
-  varieties: ["TMS 98/0581", "TME 419", "NR 8082"],
-  zones: ["Derived Savanna", "Guinea Savanna"],
-  soil: ["loam", "sandy loam"],
-  ph: [5.0, 6.5],
-  temp: [22, 34],
-  rain: [800, 1500],
-  yield: 25,
-  waterNeedScore: 2,
-  pestResistanceScore: 4,
-  fertilizer: "NPK 15-15-15 + manure",
-  season: { south: ["Mar","Apr","Sep"], north: ["May","Jun"] }
-},
+  { name:"Banana", temp:[20,35], rain:[1200,2500], ph:[5.5,7.0], zones:["Rainforest"], yield:30, fertilizer:"NPK + Organic mulch", pestResistanceScore:2, waterNeedScore:4 },
+  { name:"Plantain", temp:[22,34], rain:[1200,2500], ph:[5.5,7.0], zones:["Rainforest"], yield:28, fertilizer:"NPK + Manure", pestResistanceScore:2, waterNeedScore:4 },
+  { name:"Pineapple", temp:[20,30], rain:[800,1500], ph:[4.5,6.5], zones:["All"], yield:18, fertilizer:"NPK low dose", pestResistanceScore:4, waterNeedScore:2 },
 
-{
-  name: "Yam",
-  varieties: ["TDr 89/02665", "Meccakusa"],
-  zones: ["Derived Savanna"],
-  soil: ["loam"],
-  ph: [5.0, 6.8],
-  temp: [22, 33],
-  rain: [900, 1500],
-  yield: 18,
-  waterNeedScore: 2,
-  pestResistanceScore: 3,
-  fertilizer: "Organic manure + NPK",
-  season: { south: ["Feb","Mar"], north: ["Apr"] }
-},
+  { name:"Oil Palm", temp:[24,32], rain:[1500,3000], ph:[4.5,6.5], zones:["Rainforest"], yield:20, fertilizer:"Palm nutrient mix", pestResistanceScore:3, waterNeedScore:4 },
+  { name:"Cocoa", temp:[20,32], rain:[1200,2500], ph:[5.0,7.0], zones:["Rainforest"], yield:1.5, fertilizer:"Organic + NPK light", pestResistanceScore:3, waterNeedScore:4 },
+  { name:"Coffee", temp:[18,28], rain:[1000,2000], ph:[5.0,6.5], zones:["Highland"], yield:1.2, fertilizer:"NPK + Organic compost", pestResistanceScore:3, waterNeedScore:3 },
 
-{
-  name: "Sweet Potato",
-  varieties: ["UMUSPO 3", "UMUSPO 1"],
-  zones: ["All"],
-  soil: ["sandy loam"],
-  ph: [5.0, 6.5],
-  temp: [20, 32],
-  rain: [600, 1200],
-  yield: 12,
-  waterNeedScore: 1,
-  pestResistanceScore: 3,
-  fertilizer: "NPK 15-15-15",
-  season: { south: ["Mar","Sep"], north: ["May","Jun"] }
-},
+  { name:"Lettuce", temp:[10,25], rain:[600,1000], ph:[6.0,7.0], zones:["All"], yield:5, fertilizer:"NPK light + compost", pestResistanceScore:2, waterNeedScore:3 },
+  { name:"Cabbage", temp:[10,25], rain:[700,1200], ph:[6.0,7.5], zones:["All"], yield:12, fertilizer:"NPK + Boron", pestResistanceScore:2, waterNeedScore:3 },
+  { name:"Carrot", temp:[15,25], rain:[600,1000], ph:[6.0,7.0], zones:["All"], yield:10, fertilizer:"NPK low nitrogen", pestResistanceScore:3, waterNeedScore:2 },
 
-/* ================= CEREALS ================= */
+  { name:"Onion", temp:[15,30], rain:[500,900], ph:[6.0,7.5], zones:["Savanna"], yield:8, fertilizer:"NPK + Sulphur", pestResistanceScore:2, waterNeedScore:2 },
+  { name:"Garlic", temp:[15,30], rain:[500,900], ph:[6.0,7.5], zones:["Savanna"], yield:6, fertilizer:"NPK low dose", pestResistanceScore:4, waterNeedScore:2 },
 
-{
-  name: "Maize",
-  varieties: ["SAMMAZ 29", "Oba Super", "SAMMAZ 51"],
-  zones: ["All"],
-  soil: ["loam"],
-  ph: [5.5, 7.0],
-  temp: [20, 32],
-  rain: [700, 1200],
-  yield: 5.5,
-  waterNeedScore: 2,
-  pestResistanceScore: 3,
-  fertilizer: "NPK + Urea",
-  season: { south: ["Mar","Apr","Aug"], north: ["May","Jun"] }
-},
+  { name:"Sugarcane", temp:[20,35], rain:[1200,2500], ph:[6.0,7.5], zones:["All"], yield:80, fertilizer:"NPK high + Urea", pestResistanceScore:2, waterNeedScore:4 },
+  { name:"Sesame", temp:[25,35], rain:[300,800], ph:[5.5,7.5], zones:["Savanna"], yield:1.2, fertilizer:"NPK light", pestResistanceScore:5, waterNeedScore:1 },
+  { name:"Bitter Leaf", temp:[20,30], rain:[800,1500], ph:[5.5,7.0], zones:["All"], yield:6, fertilizer:"Organic manure", pestResistanceScore:4, waterNeedScore:2 },
 
-{
-  name: "Rice",
-  varieties: ["FARO 44", "NERICA 8", "FARO 52"],
-  zones: ["All"],
-  soil: ["clay", "loam"],
-  ph: [5.0, 7.5],
-  temp: [24, 35],
-  rain: [1000, 2000],
-  yield: 4.5,
-  waterNeedScore: 3,
-  pestResistanceScore: 2,
-  fertilizer: "NPK + Urea split",
-  season: { south: ["Apr"], north: ["Jun"] }
-},
+  { name:"Spinach", temp:[15,25], rain:[600,1000], ph:[6.0,7.5], zones:["All"], yield:7, fertilizer:"NPK light", pestResistanceScore:3, waterNeedScore:2 },
+  { name:"Amaranth", temp:[20,30], rain:[600,1200], ph:[6.0,7.5], zones:["All"], yield:6, fertilizer:"NPK + Compost", pestResistanceScore:3, waterNeedScore:2 },
 
-{
-  name: "Sorghum",
-  varieties: ["SAMSORG 40", "CSR 01"],
-  zones: ["Sudan Savanna"],
-  soil: ["sandy"],
-  ph: [5.5, 7.5],
-  temp: [22, 38],
-  rain: [400, 800],
-  yield: 3,
-  waterNeedScore: 1,
-  pestResistanceScore: 5,
-  fertilizer: "NPK 100kg/ha",
-  season: { north: ["May","Jun"] }
-},
+  { name:"Melon", temp:[25,35], rain:[500,900], ph:[6.0,7.5], zones:["Savanna"], yield:4, fertilizer:"NPK + Potassium", pestResistanceScore:3, waterNeedScore:2 },
+  { name:"Ginger", temp:[20,30], rain:[1200,2000], ph:[5.5,6.5], zones:["Rainforest"], yield:10, fertilizer:"Organic + NPK", pestResistanceScore:2, waterNeedScore:3 },
 
-{
-  name: "Millet",
-  varieties: ["SOSAT-C88"],
-  zones: ["Sudan Savanna"],
-  soil: ["sandy"],
-  ph: [5.5, 8.0],
-  temp: [26, 42],
-  rain: [300, 600],
-  yield: 1.5,
-  waterNeedScore: 1,
-  pestResistanceScore: 5,
-  fertilizer: "Microdose NPK",
-  season: { north: ["Jun","Jul"] }
-},
+  { name:"Turmeric", temp:[20,30], rain:[1200,2000], ph:[5.5,6.5], zones:["Rainforest"], yield:8, fertilizer:"Organic compost", pestResistanceScore:3, waterNeedScore:3 },
+  { name:"Tea", temp:[15,25], rain:[1500,3000], ph:[5.0,6.5], zones:["Highland"], yield:2, fertilizer:"Organic + NPK", pestResistanceScore:4, waterNeedScore:4 },
 
-/* ================= LEGUMES ================= */
+  { name:"Rubber", temp:[25,35], rain:[1500,3000], ph:[4.5,6.5], zones:["Rainforest"], yield:15, fertilizer:"NPK + Organic mulch", pestResistanceScore:3, waterNeedScore:4 },
+  { name:"Cotton", temp:[25,35], rain:[500,1200], ph:[5.5,7.5], zones:["Savanna"], yield:2.5, fertilizer:"NPK + Urea", pestResistanceScore:2, waterNeedScore:3 },
 
-{
-  name: "Cowpea",
-  varieties: ["SAMPEA 20-T", "IT97K-499-35"],
-  zones: ["All"],
-  soil: ["loam"],
-  ph: [5.0, 7.2],
-  temp: [22, 40],
-  rain: [400, 800],
-  yield: 1.5,
-  waterNeedScore: 1,
-  pestResistanceScore: 4,
-  fertilizer: "SSP + Rhizobium",
-  season: { south: ["Apr","Aug"], north: ["May"] }
-},
-
-{
-  name: "Groundnut",
-  varieties: ["SAMNUT 24", "SAMNUT 22"],
-  zones: ["All"],
-  soil: ["sandy loam"],
-  ph: [5.2, 7.2],
-  temp: [22, 38],
-  rain: [500, 900],
-  yield: 1.6,
-  waterNeedScore: 1,
-  pestResistanceScore: 3,
-  fertilizer: "SSP + Gypsum",
-  season: { south: ["Apr"], north: ["May"] }
-},
-
-{
-  name: "Soybean",
-  varieties: ["TGX 1987-62F", "TGX 1448-2E"],
-  zones: ["Guinea Savanna"],
-  soil: ["loam"],
-  ph: [5.8, 7.0],
-  temp: [20, 35],
-  rain: [700, 1200],
-  yield: 2.2,
-  waterNeedScore: 2,
-  pestResistanceScore: 3,
-  fertilizer: "Inoculant + NPK",
-  season: { north: ["May","Jun"] }
-},
-
-/* ================= VEGETABLES ================= */
-
-{
-  name: "Tomato",
-  varieties: ["Roma VF", "UC82B", "Tropimech"],
-  zones: ["All"],
-  soil: ["loam"],
-  ph: [5.5, 7.5],
-  temp: [20, 30],
-  rain: [600, 1000],
-  yield: 20,
-  waterNeedScore: 2,
-  pestResistanceScore: 2,
-  fertilizer: "NPK + Calcium",
-  season: { south: ["Oct","Nov"], north: ["Nov","Dec"] }
-},
-
-{
-  name: "Pepper",
-  varieties: ["Tatase", "Shombo", "Atarodo"],
-  zones: ["All"],
-  soil: ["loam"],
-  ph: [5.5, 7.0],
-  temp: [20, 32],
-  rain: [600, 1000],
-  yield: 10,
-  waterNeedScore: 2,
-  pestResistanceScore: 2,
-  fertilizer: "NPK + organic manure",
-  season: { south: ["Sep"], north: ["Nov"] }
-},
-
-{
-  name: "Onion",
-  varieties: ["Red Creole", "Texas Grano"],
-  zones: ["Northern"],
-  soil: ["sandy loam"],
-  ph: [6.0, 7.5],
-  temp: [15, 30],
-  rain: [500, 800],
-  yield: 12,
-  waterNeedScore: 1,
-  pestResistanceScore: 3,
-  fertilizer: "NPK 15-15-15",
-  season: { north: ["Nov","Dec"] }
-},
-
-{
-  name: "Cucumber",
-  varieties: ["Marketmore", "Poinsett"],
-  zones: ["All"],
-  soil: ["loam"],
-  ph: [5.5, 7.0],
-  temp: [20, 30],
-  rain: [600, 1000],
-  yield: 12,
-  waterNeedScore: 2,
-  pestResistanceScore: 2,
-  fertilizer: "NPK",
-  season: { all: ["Dry season irrigation"] }
-},
-
-{
-  name: "Watermelon",
-  varieties: ["Sugar Baby", "Crimson Sweet"],
-  zones: ["Northern"],
-  soil: ["sandy"],
-  ph: [5.5, 7.5],
-  temp: [25, 38],
-  rain: [400, 800],
-  yield: 15,
-  waterNeedScore: 1,
-  pestResistanceScore: 3,
-  fertilizer: "NPK",
-  season: { north: ["Dry season"] }
-},
-
-{
-  name: "Okra",
-  varieties: ["NH47-4", "Clemson Spineless"],
-  zones: ["All"],
-  soil: ["loam"],
-  ph: [5.5, 7.0],
-  temp: [22, 35],
-  rain: [500, 1000],
-  yield: 8,
-  waterNeedScore: 2,
-  pestResistanceScore: 3,
-  fertilizer: "NPK",
-  season: { all: ["Rainy season"] }
-},
-
-/* ================= TREE CROPS ================= */
-
-{
-  name: "Oil Palm",
-  varieties: ["Tenera"],
-  zones: ["Forest"],
-  soil: ["loam"],
-  ph: [5.0, 6.5],
-  temp: [22, 32],
-  rain: [1500, 3000],
-  yield: 20,
-  waterNeedScore: 3,
-  pestResistanceScore: 3,
-  fertilizer: "NPKMg",
-  season: { south: ["All year"] }
-},
-
-{
-  name: "Cocoa",
-  varieties: ["CRIN CF9"],
-  zones: ["Forest"],
-  soil: ["loam"],
-  ph: [5.0, 6.5],
-  temp: [20, 32],
-  rain: [1200, 2500],
-  yield: 1.2,
-  waterNeedScore: 3,
-  pestResistanceScore: 2,
-  fertilizer: "NPK + mulch",
-  season: { south: ["Mar","Apr"] }
-},
-
-/* ================= FRUITS ================= */
-
-{
-  name: "Banana",
-  varieties: ["Cavendish"],
-  zones: ["All"],
-  soil: ["loam"],
-  ph: [5.5, 7.0],
-  temp: [24, 35],
-  rain: [1200, 2500],
-  yield: 30,
-  waterNeedScore: 3,
-  pestResistanceScore: 3,
-  fertilizer: "High K NPK",
-  season: { south: ["All year"] }
-},
-
-{
-  name: "Mango",
-  varieties: ["Kent", "Tommy Atkins"],
-  zones: ["All"],
-  soil: ["loam"],
-  ph: [5.5, 7.5],
-  temp: [24, 40],
-  rain: [600, 1200],
-  yield: 15,
-  waterNeedScore: 1,
-  pestResistanceScore: 3,
-  fertilizer: "Compost + NPK",
-  season: { all: ["Rainy season flowering"] }
-}
-
+  { name:"Pearl Millet", temp:[28,40], rain:[200,700], ph:[5.0,7.5], zones:["Sahel"], yield:1.8, fertilizer:"Micro NPK", pestResistanceScore:5, waterNeedScore:1 },
+  { name:"Fonio", temp:[25,35], rain:[300,800], ph:[5.0,7.0], zones:["Sahel"], yield:1.0, fertilizer:"Low NPK", pestResistanceScore:5, waterNeedScore:1 }
 ];
-
-module.exports = crops;
